1372bdbe7ef159f603110b4a355cce225a99d99c
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-05-06 16:50:38 -0300

    renamed tests, added placeholder for running time and memory consumption check tests

--- test/test_max_pairwise.py
+++ test/test_max_pairwise.py
@@ -6,10 +6,10 @@ import heapq
 
 class TestMaxPairwiseProduct(TestCase):
 
-    def test_zero_on_empty(self):
+    def test_zero_on_empty_sequence(self):
         assert max_pairwise_prod([]) == 0
 
-    def test_zero_on_one_element(self):
+    def test_zero_on_one_element_sequence(self):
         assert max_pairwise_prod([1]) == 0
 
     def test_min_seq(self):
@@ -29,3 +29,9 @@ class TestMaxPairwiseProduct(TestCase):
         seq = [random.randint(0, MAX_VALUE) for _ in range(MAX_SEQ_LEN)]
         largest = heapq.nlargest(2, seq)
         assert max_pairwise_prod(seq) == largest[0] * largest[1]
+
+    def test_running_time(self):
+        pass
+
+    def test_memory_usage(self):
+        pass
