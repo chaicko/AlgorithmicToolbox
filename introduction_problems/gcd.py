ec097914f3f89a5f20b868fc0f3e4ae00e90d686
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-05-10 16:12:44 -0300

    fixed GCD to run quick

--- introduction_problems/gcd.py
+++ introduction_problems/gcd.py
@@ -5,13 +5,9 @@ MAX_GCD_OPERAND_VALUE = 2 * 10 ** 9
 
 
 def gcd(a, b):
-    current_gcd = 1
-    for d in range(2, min(a, b) + 1):
-        if a % d == 0 and b % d == 0:
-            if d > current_gcd:
-                current_gcd = d
-
-    return current_gcd
+    if b == 0:
+        return a
+    return gcd(b, a % b)
 
 
 if __name__ == "__main__":
