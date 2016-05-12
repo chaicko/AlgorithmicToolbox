985492bd577bcc0eb4ea44bda5c90d7527f0cdc4
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-05-12 10:24:11 -0300

    Fixed LCM

--- introduction_problems/lcm.py
+++ introduction_problems/lcm.py
@@ -2,9 +2,15 @@
 import sys
 
 
+def gcd(a, b):
+    if b == 0:
+        return a
+    return gcd(b, a % b)
+
+
 def lcm(a, b):
-    #write your code here
-    return a*b
+    d = gcd(a, b)
+    return (a * b) / d
 
 if __name__ == '__main__':
     input = sys.stdin.read()
