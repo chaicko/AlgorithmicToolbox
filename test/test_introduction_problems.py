0ba0f255d027a0ba06c122bdb0faddd61b60fdd2
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-05-12 19:31:48 -0300

    migrated all to py.test, added coverage and coveralls for travis

--- test/test_introduction_problems.py
+++ test/test_introduction_problems.py
@@ -1,9 +1,9 @@
 from unittest import TestCase
-from introduction_problems.fib import *
-from introduction_problems.fibonacci_last_digit import *
-from introduction_problems.gcd import *
+from introduction_problems.fib import calc_fib
+from introduction_problems.fibonacci_last_digit import \
+    get_fibonacci_last_digit, FIBONACCI_LAST_DIGIT_MAX_VALUE
+from introduction_problems.gcd import gcd
 from introduction_problems.lcm import lcm
-from nose.tools import eq_, timed
 
 
 def fib(n):
@@ -15,50 +15,49 @@ def fib(n):
 
 class TestSmallFibonacci(TestCase):
     def test_fib_0_is_0(self):
-        eq_(calc_fib(0), 0)
+        assert calc_fib(0) == 0
 
     def test_fib_1_is_1(self):
-        eq_(calc_fib(1), 1)
+        assert calc_fib(1) == 1
 
     def test_fib_first_45(self):
         for index, fibo in enumerate(fib(45)):
             res = calc_fib(index)
-            eq_(res, fibo, "fib(%d)=%d != %d" % (index, res, fibo))
+            assert res == fibo
 
 
 class TestFibonacciLastDigit(TestCase):
     def test_fib_0_is_0(self):
-        eq_(get_fibonacci_last_digit(0), 0)
+        assert get_fibonacci_last_digit(0) == 0
 
     def test_fib_1_is_1(self):
-        eq_(get_fibonacci_last_digit(1), 1)
+        assert get_fibonacci_last_digit(1) == 1
 
     def test_fib_all(self):
         for i, f in enumerate(fib(FIBONACCI_LAST_DIGIT_MAX_VALUE)):
             r = get_fibonacci_last_digit(i)
             c = f % 10
-            eq_(r, c, "get_fibonacci_last_digit(%d)=%d != %d" % (i, r, c))
+            assert r == c
 
 
 class TestGCD(TestCase):
     def test_sample1(self):
-        eq_(gcd(18, 35), 1)
+        assert gcd(18, 35) == 1
 
     def test_sample2(self):
-        eq_(gcd(28851538, 1183019), 17657)
+        assert gcd(28851538, 1183019) == 17657
 
-    @timed(5)
     def test_max_values(self):
-        eq_(gcd(2000000000, 1999999999), 1)
+        assert gcd(2000000000, 1999999999) == 1
 
 
 class TestLCM(TestCase):
     def test_sample1(self):
         l = lcm(6, 8)
         assert not isinstance(l, float)
-        eq_(l, 24)
+        assert l == 24
 
     def test_sample2(self):
         l = lcm(28851538, 1183019)
         assert not isinstance(l, float)
-        eq_(l, 1933053046)
+        assert l == 1933053046
