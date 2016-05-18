99b94dcb812a080194428fc4c6e5d34b40e2d79b
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-05-18 16:21:50 -0300

    move main code to its main function

--- greedy/fractional_knapsack.py
+++ greedy/fractional_knapsack.py
@@ -16,10 +16,14 @@ def get_optimal_value(capacity, weights, values):
     return value
 
 
-if __name__ == "__main__":
+def main():
     data = list(map(int, sys.stdin.read().split()))
     n, capacity = data[0:2]
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
+
+
+if __name__ == "__main__":
+    main()
