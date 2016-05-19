1d16eed1e80e41cb8ecaa2a265442afcce793f7f
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-05-19 20:20:05 -0300

    move main code to its main function

--- greedy/dot_product.py
+++ greedy/dot_product.py
@@ -1,6 +1,6 @@
 #Uses python3
-
 import sys
+
 
 def min_dot_product(a, b):
     #write your code here
@@ -9,11 +9,15 @@ def min_dot_product(a, b):
         res += a[i] * b[i]
     return res
 
-if __name__ == '__main__':
+
+def main():
     input = sys.stdin.read()
     data = list(map(int, input.split()))
     n = data[0]
     a = data[1:(n + 1)]
     b = data[(n + 1):]
     print(min_dot_product(a, b))
-    
+
+
+if __name__ == '__main__':
+    main()
