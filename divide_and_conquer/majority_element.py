53924ff7589e04e34dc126f8bc745b5061ee4f5e
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-05-24 19:44:06 -0300

    Adding the Divide and Conquer (Week3) Python starter files modifying them to use a main() function as entry point (used on tests)

---
+++ divide_and_conquer/majority_element.py
@@ -0,0 +1,24 @@
+# Uses python3
+import sys
+
+
+def get_majority_element(a, left, right):
+    if left == right:
+        return -1
+    if left + 1 == right:
+        return a[left]
+    # write your code here
+    return -1
+
+
+def main():
+    input = sys.stdin.read()
+    n, *a = list(map(int, input.split()))
+    if get_majority_element(a, 0, n) != -1:
+        print(1)
+    else:
+        print(0)
+
+
+if __name__ == '__main__':
+    main()
