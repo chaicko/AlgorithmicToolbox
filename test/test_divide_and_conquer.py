cde8ff24c46da80f0953c32b40005ec4f5f3e80a
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-05-24 23:00:02 -0300

    Adding example test from PDF for binary search as a parametrized main_runner test

--- test/test_divide_and_conquer.py
+++ test/test_divide_and_conquer.py
@@ -9,19 +9,11 @@ import pytest
 @pytest.mark.timeout(10)  # 10 seconds timeout for binary this tests
 class TestBinarySearch:
     @pytest.mark.parametrize("test_input,expected", [
-        ("3+5", 8),
-        ("2+4", 6),
-        ("6*9", 42),
+        (([5, 1, 5, 8, 12, 13], [5, 8, 1, 23, 1, 11]), "2 0 -1 0 -1")
     ])
-    def tst(self, test_input, expected, main_runner):
-        main_runner.run(test_input, expected)
-
-    @pytest.mark.skip(reason="TODO")
-    def test_sample1(self, mock_stdin, capfd):
-        mock_stdin.setvalue([5, 1, 5, 8, 12, 13], [5, 8, 1, 23, 1, 11])
-        binary_search.main()
-        out, err = capfd.readouterr()
-        assert "2 0 -1 0 -1" in out
+    def test_sample(self, test_input, expected, main_runner):
+        result = main_runner(binary_search, test_input)
+        assert expected in result
 
 
 @pytest.mark.skip(reason="TODO")
