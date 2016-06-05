832a6aa8739313a007ee48c8e5f426084bf81073
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-06-05 19:13:09 -0300

    Knapsack + edit distance passing on grader.

--- dynprog/knapsack.py
+++ dynprog/knapsack.py
@@ -24,11 +24,7 @@ def optimal_weight(W, w):
     return K[W][n]
 
 
-def main():
+if __name__ == '__main__':
     input = sys.stdin.read()
     W, n, *w = list(map(int, input.split()))
     print(optimal_weight(W, w))
-
-
-if __name__ == '__main__':
-    main()
