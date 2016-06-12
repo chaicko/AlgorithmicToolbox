e4e449fc12e91c67d0e145f6b3d72c0786268877
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-06-12 12:17:01 -0300

    Adding test file for basic data structures

---
+++ test/data_structures/test_basic_data_structures.py
@@ -0,0 +1,9 @@
+import data_structures.basic.check_brackets as check_brackets
+
+import pytest
+
+
+@pytest.mark.timeout(1)  # 1 second timeout for this tests
+class TestCheckBrackets:
+    def test_basic(self, main_runner):
+        pass
