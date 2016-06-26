6c3404c53776729279624f03a52a251fe1cf2310
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2016-06-26 11:06:32 -0300

    Adding assignment files and test file.

---
+++ test/data_structures/test_hash_tables.py
@@ -0,0 +1,23 @@
+import data_structures.hash_tables.hash_chains as hash_chains
+import data_structures.hash_tables.hash_substring as hash_substring
+import data_structures.hash_tables.phone_book as phone_book
+
+import pytest
+
+
+@pytest.mark.timeout(6)
+class TestPhoneBook:
+    def test_sample1(self):
+        pass
+
+
+@pytest.mark.timeout(7)
+class TestHashChains:
+    def test_sample1(self):
+        pass
+
+
+@pytest.mark.timeout(5)
+class TestHashSubstring:
+    def test_sample1(self):
+        pass
