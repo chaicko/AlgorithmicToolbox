0dd3c8372c9928fb7d18fe8bc8d4043639f3bf19
Alejandro Schwoykoski <alejandro.schwoykoski@gmail.com>
2018-01-28 20:55:25 +1100

    starting with reachability

--- graphs/reachability.py
+++ graphs/reachability.py
@@ -7,16 +7,26 @@ def reach(adj, x, y):
     return 0
 
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
+def parse_input(input):
     data = list(map(int, input.split()))
     n, m = data[0:2]
     data = data[2:]
     edges = list(zip(data[0:(2 * m):2], data[1:(2 * m):2]))
+
     x, y = data[2 * m:]
-    adj = [[] for _ in range(n)]
     x, y = x - 1, y - 1
+
+    adj = [[] for _ in range(n)]
     for (a, b) in edges:
         adj[a - 1].append(b - 1)
         adj[b - 1].append(a - 1)
+    return adj, x, y
+
+
+def main(args):
+    adj, x, y = parse_input(sys.stdin.read().strip())
     print(reach(adj, x, y))
+
+
+if __name__ == '__main__':
+    main(sys.args[1:])
